24aeba4623cdfe783f577195dbd1cfa917fbaad3
YujinJeon888888 <yujinjun@ewhain.net>
2023-11-11 11:53:02 +0900

    Add: ObjectController, GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+/// <summary>
+/// 상태관할
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    [HideInInspector] public Define.ObjectName objectName;
+    
+}
